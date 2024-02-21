4c2ca1dcde2632efc02a86261706ce28cfc25cc9
Vinoth1512 <vinothvk1211@icloud.com>
2024-02-21 15:21:06 +0530

    First Commit

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+    '''
+    This function will return list of requirements
+    from requirements.txt file
+    '''
+    # Initializing blank requirements list
+    requirements = []
+
+    # Opening the file
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        # Remove hyphen_e_dot if present in requirements
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-        return requirements
-
-
+    return requirements
 
 setup(
-    name='DiamondPriceprediction',
-    version='0.0.1',
-    author='Pwskills',
-    author_email="example@gmail.com",
-    install_requires=get_requirements('requirements.txt'),
-    packages=find_packages()
-
+    name = 'mlproject_regression',
+    version= '0.0.1',
+    author='Utkarsh Gaikwad',
+    author_email='gaikwadujg@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
 )
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,19 +11,23 @@ from sklearn.preprocessing import OrdinalEncoder,StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 import os
+
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformation_object(self):
+        '''
+        This function is responsible for data transformation
+        '''
         try:
-            logging.info('Data Transformation initiated')
+            
             # Define which columns should be ordinal-encoded and which should be scaled
             categorical_cols = ['cut', 'color','clarity']
             numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
@@ -32,43 +36,42 @@ class DataTransformation:
             cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
             color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
             clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
-            
-            logging.info('Pipeline Initiated')
 
-            ## Numerical Pipeline
-            num_pipeline=Pipeline(
-                steps=[
+            # Numerical Pipeline
+            num_pipeline = Pipeline(
+                steps = [
                 ('imputer',SimpleImputer(strategy='median')),
-                ('scaler',StandardScaler())
-
+                ('scaler',StandardScaler())                
                 ]
-
             )
 
-            # Categorigal Pipeline
-            cat_pipeline=Pipeline(
+            # Categorical Pipeline
+            cat_pipeline = Pipeline(
                 steps=[
                 ('imputer',SimpleImputer(strategy='most_frequent')),
-                ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                ('ordinal_encoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
                 ('scaler',StandardScaler())
                 ]
-
             )
 
-            preprocessor=ColumnTransformer([
-            ('num_pipeline',num_pipeline,numerical_cols),
-            ('cat_pipeline',cat_pipeline,categorical_cols)
-            ])
-            
+            logging.info(f'Categorical Columns : {categorical_cols}')
+            logging.info(f'Numerical Columns   : {numerical_cols}')
+
+            preprocessor = ColumnTransformer(
+                [
+                ('num_pipeline',num_pipeline,numerical_cols),
+                ('cat_pipeline',cat_pipeline,categorical_cols)
+                ]
+            )
+
             return preprocessor
-
-            logging.info('Pipeline Completed')
-
+        
         except Exception as e:
-            logging.info("Error in Data Trnasformation")
+            logging.info('Exception occured in Data Transformation Phase')
             raise CustomException(e,sys)
         
-    def initaite_data_transformation(self,train_path,test_path):
+    def initate_data_transformation(self,train_path,test_path):
+
         try:
             # Reading train and test data
             train_df = pd.read_csv(train_path)
@@ -90,17 +93,15 @@ class DataTransformation:
 
             input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
             target_feature_test_df=test_df[target_column_name]
-            
-            ## Trnasformating using preprocessor obj
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             logging.info("Applying preprocessing object on training and testing datasets.")
             
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-
+            
             save_object(
 
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
@@ -114,8 +115,7 @@ class DataTransformation:
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
-            
+        
         except Exception as e:
-            logging.info("Exception occured in the initiate_datatransformation")
-
+            logging.info('Exception occured in initiate_data_transformation function')
             raise CustomException(e,sys)
